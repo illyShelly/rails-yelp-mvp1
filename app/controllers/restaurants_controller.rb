32ebe2f8bb00c8dff2ade847ef6b79d94c270fbf
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-09 13:44:25 +0100

    update gemfile rename repo

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-before_action :set_restaurant, only: [:show, :edit]
+before_action :set_restaurant, only: [:show, :edit, :destroy]
 
 
   # GET /restaurants
@@ -7,33 +7,47 @@ before_action :set_restaurant, only: [:show, :edit]
     @restaurants = Restaurant.all
   end
 
-  def show
-  end
-
    # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
   end
 
-  def edit
-  end
-
   def create
     # raise
     @restaurant = Restaurant.new(restaurant_params)
-    # new http request
+   if @restaurant.save
+    # is necessary specify @restaurant.id
+     redirect_to restaurant_path(@restaurant.id)
+   else
+     render :new
+   end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @restaurant.destroy
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
+
   def set_restaurant
       @restaurant = Restaurant.find(params[:id])
   end
 
-
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :content)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
-
 end
+
+
+# < link_to "Delete restaurant", restaurant_path(dddd), method: :DELETE,
+#     data: { confirm: "Are you sure? There is not step back." } >
+
+
